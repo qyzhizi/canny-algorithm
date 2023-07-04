26b624cc1c8ce64a0322d664c4440620b09826a0
qyzhizi <2830942138@qq.com>
2023-07-04 13:51:25 +0800

    add hysteresis_threshold

--- conv2d-canny.py
+++ conv2d-canny.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import math
 import time
 import argparse
+from collections import deque
 
 
 def dnorm(x, mu, sd):
@@ -94,19 +95,10 @@ def functional_conv2d_vertical(im, verbose=False):
     return edge_detect
 
 
-def threshold(image, low, high, weak=0, strong=255):
-    strong_mask = image >= high
-    weak_mask = (image > low) & (image < high)
-    output = torch.zeros_like(image)
-    output[strong_mask] = strong
-    output[weak_mask] = weak
-    return output
-
-
-def nms(gradient_magnitude, gradient_direction_quantized, verbose=False):
+def nms(gradient_magnitude: torch.tensor, gradient_direction_quantized: torch.tensor, verbose=False) -> torch.tensor:
     """非极大值抑制"""
 
-    nms_tensor = torch.zeros_like(gradient_magnitude)
+    nms_edge = torch.zeros_like(gradient_magnitude)
     # nms_conv_op = torch.nn.Conv2d(in_channels=1,out_channels=3, kernel_size=3, stride=1, padding=1, padding_mode='replicate', bias=False)
     nms_conv_op = torch.nn.Conv2d(in_channels=1,out_channels=3, kernel_size=3, stride=1, padding=1, bias=False)
 
@@ -138,7 +130,7 @@ def nms(gradient_magnitude, gradient_direction_quantized, verbose=False):
         
         # selected_gradient_magnitude = gradient_magnitude * mask_i
         # print("selected: ", selected_gradient_magnitude)
-        # nms_tensor_i = torch.nn.functional.max_pool2d(selected_gradient_magnitude, kernel_size=3,
+        # nms_edge_i = torch.nn.functional.max_pool2d(selected_gradient_magnitude, kernel_size=3,
         #                                               stride=1, padding=1)
         nms_conv_op.weight.data = kernel_direction_quantized[i].reshape((3, 1, 3, 3))
         nms_direction_conv_i = nms_conv_op(gradient_magnitude)
@@ -148,21 +140,21 @@ def nms(gradient_magnitude, gradient_direction_quantized, verbose=False):
         if verbose:
             print("nms_direction_conv_mask_max_i: ", nms_direction_conv_mask_max_i)
             print("nms_direction_i: ", nms_direction_conv_mask_max_i)
-        nms_tensor = nms_tensor + ((gradient_magnitude == nms_direction_conv_mask_max_i) * nms_direction_conv_mask_max_i) 
+        nms_edge = nms_edge + ((gradient_magnitude == nms_direction_conv_mask_max_i) * nms_direction_conv_mask_max_i) 
 
     if verbose:
-        print("nms_tensor: ", nms_tensor)
+        print("nms_edge: ", nms_edge)
 
     # verbose = True
     if verbose:
-        plt.imshow(nms_tensor.squeeze().detach().numpy(), cmap='gray')
+        plt.imshow(nms_edge.squeeze().detach().numpy(), cmap='gray')
         plt.title("Non-Maximum Suppression")
         plt.show()
     
-    return nms_tensor
+    return nms_edge
 
 
-def get_gradient_magnitude(gradient_v, gradient_h, verbose=False, use_l2_norm=False):
+def get_gradient_magnitude(gradient_v: torch.tensor, gradient_h: torch.tensor, verbose=False, use_l2_norm=False) -> torch.tensor:
     """计算梯度幅值
     """
     # 计算梯度的幅值
@@ -182,7 +174,7 @@ def get_gradient_magnitude(gradient_v, gradient_h, verbose=False, use_l2_norm=Fa
     return gradient_magnitude
 
 
-def get_gradient_direction_quantized(gradient_v, gradient_h, verbose=False):
+def get_gradient_direction_quantized(gradient_v: torch.tensor, gradient_h: torch.tensor, verbose=False) -> torch.tensor:
     """计算梯度方向
     将梯度方向转换为四个方向之一: 0度，45度，90度，135度, 分别用 0,1,2,3 表示。
     """
@@ -200,6 +192,47 @@ def get_gradient_direction_quantized(gradient_v, gradient_h, verbose=False):
         print("gradient_direction_quantized: ", gradient_direction_quantized)
 
     return gradient_direction_quantized
+
+
+def threshold(image, low, high, weak=0, strong=255):
+    strong_mask = image >= high
+    weak_mask = (image > low) & (image < high)
+    output = torch.zeros_like(image)
+    output[strong_mask] = strong
+    output[weak_mask] = weak
+    return output
+
+
+def hysteresis_threshold(nms_edge: torch.tensor, low: int, high: int) -> torch.tensor:
+    nms_edge = nms_edge.squeeze()
+    strong_mask = nms_edge >= high
+    weak_mask = (nms_edge > low) & (nms_edge < high).int()
+    output_index = []
+    # get strong_mask index
+    strong_index = torch.nonzero(strong_mask).tolist()
+    # create stack and push strong_index
+    stack = deque(strong_index)
+
+    while len(stack) > 0:
+        # pop item
+        item = stack.pop()
+        # get item index
+        i, j = item[0], item[1]
+        # get weak_mask index
+        local_index = torch.nonzero(weak_mask[i-1:i+2, j-1:j+2]).tolist()
+        for local_index_item in local_index:
+            # stack.append(weak_index[i])
+            weak_index_item = [i-1+local_index_item[0], j-1+local_index_item[1]]
+            stack.append(weak_index_item)
+            weak_mask[weak_index_item[0], weak_index_item[1]] = 0
+        output_index.append(item)
+
+    # create output tensor use output_index
+    output_index_tensor = torch.tensor(output_index)  # (N, 2) 
+    output = torch.zeros_like(nms_edge)
+    output[output_index_tensor[:, 0], output_index_tensor[:, 1]] = 255
+
+    return output
 
 
 def save_edge_detect_as_image(edge_detect, save_image_path, verbose=False):
@@ -290,9 +323,12 @@ def main():
     # 计算梯度方向
     gradient_direction_quantized = get_gradient_direction_quantized(gradient_v, gradient_h, verbose=verbose)
     # 非极大值抑制
-    nms_tensor = nms(gradient_magnitude, gradient_direction_quantized, verbose=verbose)
+    nms_edge = nms(gradient_magnitude, gradient_direction_quantized, verbose=verbose)
+
     # 阈值处理
-    edge_detect = threshold(nms_tensor, low=threshold_low, high=threshold_high, weak=50, strong=255)
+    # edge_detect = threshold(nms_edge, low=threshold_low, high=threshold_high, weak=50, strong=255)
+    # 滞后阈值
+    edge_detect = hysteresis_threshold(nms_edge, low=threshold_low, high=threshold_high)
 
     if verbose:
         print("edge_detect.shape: ", edge_detect.shape, "im.shape: ", im.shape)
